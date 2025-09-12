9b842c4447540e7dfe94c2b0f9fd1cc2fda76958
minhhiep02112001 <chuminhhiep0211@gmail.com>
2025-09-13 00:11:47 +0700

    update code

--- resources/views/front_end/home.blade.php
+++ resources/views/front_end/home.blade.php
@@ -809,7 +809,7 @@
             <div class="border-t border-red-200 pt-8">
                 <div class="flex flex-col md:flex-row items-center gap-6">
                     <img src="./assets/uploads/logo_footer.png" alt="The Best Wine Logo"
-                        class="w-40 h-40 object-contain shadow-md">
+                        class=" object-contain shadow-md">
                     <div class="text-red-800 text-lg leading-relaxed">
                         <p class="mb-4">The Best Wine luôn là công ty tiên phong và dẫn đầu về truyền bá nghệ thuật
                             thưởng thức rượu vang và văn hóa giao tiếp trên bàn tiệc, và là thương hiệu, địa chỉ tin cậy
